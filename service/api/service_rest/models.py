a71d1114e4cd1368ae5f671c60965971d09e0a59
Ni <evelyn.z.mccoy@gmail.com>
2023-07-24 13:41:17 -0700

    make models

--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+class Techinican(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    employee_id = models.CharField(max_length=200)
+
+
+class AutomobileVO(models.Model):
+    vin = models.CharField(max_length=200)
+    sold = models.BooleanField(default=False)
+
+
+class Appointment(models.Model):
+    date_time = models.DateTimeField()
+    reason = models.CharField(max_length=200)
+    status = models.CharField(max_length=200, default="Created")
+    vin = models.CharField(max_length=200)
+    customer = models.CharField(max_length=200)
+    technician = models.ForeignKey(
+        Techinican,
+        related_name="technician",
+        on_delete=models.CASCADE,
+    )
